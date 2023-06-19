39a5400c6c12cdb330f67327dfdda76c302ad722
Deagle50 <urko.urbieta@inetum.com>
2023-06-19 13:33:52 +0200

    Todo funcionando, Urko lanza bomba de humo en Inetum

--- app.js
+++ app.js
@@ -6,33 +6,49 @@ var jsonParser = bodyParser.json();
 
 const cors = require("cors");
 
-const capitanes = [
-  { nombre: "David", color: "" },
-  { nombre: "Yosu", color: "" },
-];
+/**
+ *
+ * 
+ */
 
 const jugadores = [
-  { nombre: "Urko", color: "" },
-  { nombre: "Maestre", color: "" },
-  { nombre: "Sanjo", color: "" },
-  { nombre: "Edu", color: "" },
-  { nombre: "Yaiza", color: "" },
-  { nombre: "Manex", color: "" },
-  { nombre: "Gorka", color: "" },
-  { nombre: "Cynda", color: "" },
-  { nombre: "Larra", color: "" },
-  { nombre: "Nahia", color: "" },
-  { nombre: "Borja", color: "" },
-  { nombre: "Ander", color: "" },
-  { nombre: "Kulentxo", color: "" },
-  { nombre: "Jenni", color: "" },
+  [
+    { nombre: "Maestre", color: "negro" },
+    { nombre: "Alexander", color: "blanco" },
+  ],
+  [
+    { nombre: "Ivan", color: "" },
+    { nombre: "Davos", color: "" },
+  ],
+  [
+    { nombre: "Sanjo", color: "" },
+    { nombre: "Totx", color: "" },
+  ],
+  [
+    { nombre: "Manex", color: "" },
+    { nombre: "Edu", color: "" },
+  ],
+  [
+    { nombre: "Jorge", color: "" },
+    { nombre: "Urko", color: "" },
+  ],
+  [
+    { nombre: "Unai M", color: "" },
+    { nombre: "Gorka", color: "" },
+  ],
+  [
+    { nombre: "Borja", color: "" },
+    { nombre: "Deivo", color: "" },
+  ],
 ];
 
 var app = express();
 app.use(cors());
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const port = 4646;
+
+app.listen(port, () => {
+  console.log("Server running on port " + port);
 });
 
 app.get("/jugadores", (req, res, next) => {
@@ -41,7 +57,8 @@ app.get("/jugadores", (req, res, next) => {
 
 app.get("/reiniciar", (req, res, next) => {
   jugadores.forEach((element) => {
-    element.color = "";
+    element[0].color = "";
+    element[1].color = "";
   });
   res.json("OK");
 });
@@ -50,11 +67,36 @@ app.post("/cambiar", jsonParser, (req, res, next) => {
   console.log(req.body);
   let nombre = req.body.nombre;
   let color = req.body.color;
-  let indice = buscar(nombre);
-  jugadores[indice].color = color;
+  let indices = buscar(nombre);
+  console.log("INDICES");
+  console.log(indices);
+  jugadores[indices[0]][indices[1]].color = color;
+  console.log(jugadores);
   res.json(jugadores);
 });
 
+app.get("/sorteo", (req, res, next) => {
+  res.json(sorteo());
+});
+
 function buscar(nombre) {
-  return jugadores.findIndex((o) => o.nombre === nombre);
+  for (let i = 0; i < jugadores.length; i++) {
+    if (jugadores[i][0].nombre == nombre) {
+      return [i, 0];
+    }
+    if (jugadores[i][1].nombre == nombre) {
+      return [i, 1];
+    }
+  }
+  // return jugadores.findIndex((o) => o.nombre === nombre);
+}
+
+const sorteo = () => {
+  return getRandomInt(1, 2);
+};
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
